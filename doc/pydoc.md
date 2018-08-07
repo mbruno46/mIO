d816d1c5b94b9e8cd4c1fb19a592d44de5b27141
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2018-08-07 15:00:55 -0400

    python doc expanded; python reader has more features

--- doc/pydoc.md
+++ doc/pydoc.md
@@ -1,3 +1,51 @@
 # mIO documentation - Python
 
+## Reading
 
+The flexibility of python allows the user
+to read the fiels contained in a dat file
+pretty easily
+
+```python
+from mIO import mIO
+
+f = mIO("filename-here")
+myvec = f.read("myvec")
+```
+
+When the class is initialized little endianess
+is automatically assumed. If the dat file has
+been written in big endianess the user can
+specify it with
+
+```python
+f = mIO("filename-here",'>')
+```
+
+In general the function read will return a 
+list containing every instance of the field
+corresponding to the input tag. Each element
+of the list will be a tuple whose size
+depends on the size of the field
+
+```python
+vec = f.read("myvec")
+print vec[0] 
+print vec[1] 
+```
+
+It is possible to write to ouput the entire
+content of the dat file by using the built-in
+function print
+
+```python
+f = mIO("filename-here")
+print f
+```
+
+It is possible to inspect the content of the dat
+file, for example by printing all the tags
+
+```python
+print f.fields()
+```
--- python/mIO.py
+++ python/mIO.py
@@ -1,30 +1,38 @@
 import struct
 
 class mIO:
-    def __init__(self,name):
+    def __init__(self,name,endian='<'):
         self.fname = name
+        self.endian = endian
 
-    def read(self,tag):
+    def read(self,tag,idx=None):
         res = []
+        count = 0
         with open(self.fname,'rb') as f:
             while True:
                 bb = f.read(4)
                 if not bb:
                     break
                 else:
-                    [n] = struct.unpack('i',bb)
-                tag_r = struct.unpack('c'*n,f.read(n))
-                (type_r,) = struct.unpack('c',f.read(1))
-                [n] = struct.unpack('i',f.read(4))
+                    [n] = struct.unpack(self.endian+'i',bb)
+                tag_r = struct.unpack(self.endian+'c'*n,f.read(n))
+                (type_r,) = struct.unpack(self.endian+'c',f.read(1))
+                [n] = struct.unpack(self.endian+'i',f.read(4))
                 if type_r is 'c':
                     var = str(f.read(n))
                 elif type_r is 'i':
-                    var = struct.unpack('i'*n,f.read(4*n))
+                    var = struct.unpack(self.endian+'i'*n,f.read(4*n))
                 elif type_r is 'd':
-                    var = struct.unpack('d'*n,f.read(8*n))
+                    var = struct.unpack(self.endian+'d'*n,f.read(8*n))
                 
                 if tag==''.join(tag_r):
-                    res.append( var )
+                    if idx==None:
+                        res.append( var )
+                    else:
+                        if count==idx:
+                            res.append( var )
+                            break
+                    count += 1
         return res
 
     def __str__(self):
@@ -35,18 +43,18 @@ class mIO:
                 if not bb:
                     break
                 else:
-                    [n] = struct.unpack('i',bb)
-                tag_r = struct.unpack('c'*n,f.read(n))
-                (type_r,) = struct.unpack('c',f.read(1))
-                [n] = struct.unpack('i',f.read(4))
+                    [n] = struct.unpack(self.endian+'i',bb)
+                tag_r = struct.unpack(self.endian+'c'*n,f.read(n))
+                (type_r,) = struct.unpack(self.endian+'c',f.read(1))
+                [n] = struct.unpack(self.endian+'i',f.read(4))
                 res += '\n' + ''.join(tag_r) + ' = '
                 if type_r is 'c':
                     res += ' ' + str(f.read(n))
                 else:
                     if type_r is 'i':
-                        var = struct.unpack('i'*n,f.read(4*n))
+                        var = struct.unpack(self.endian+'i'*n,f.read(4*n))
                     elif type_r is 'd':
-                        var = struct.unpack('d'*n,f.read(8*n))
+                        var = struct.unpack(self.endian+'d'*n,f.read(8*n))
                     for v in var:
                         res += ' ' + str(v) 
         return res
@@ -59,10 +67,10 @@ class mIO:
                 if not bb:
                     break
                 else:
-                    [n] = struct.unpack('i',bb)
-                tag_r = struct.unpack('c'*n,f.read(n))
-                (type_r,) = struct.unpack('c',f.read(1))
-                [n] = struct.unpack('i',f.read(4))
+                    [n] = struct.unpack(self.endian+'i',bb)
+                tag_r = struct.unpack(self.endian+'c'*n,f.read(n))
+                (type_r,) = struct.unpack(self.endian+'c',f.read(1))
+                [n] = struct.unpack(self.endian+'i',f.read(4))
                 res += '\n - ' + ''.join(tag_r) 
                 if type_r is 'c':
                     f.seek(n,1)
@@ -71,3 +79,26 @@ class mIO:
                 elif type_r is 'd':
                     f.seek(8*n,1)
         return res
+    
+    def howmany(self,tag):
+        count = 0
+        with open(self.fname,'rb') as f:
+            while True:
+                bb = f.read(4)
+                if not bb:
+                    break
+                else:
+                    [n] = struct.unpack(self.endian+'i',bb)
+                tag_r = struct.unpack(self.endian+'c'*n,f.read(n))
+                (type_r,) = struct.unpack(self.endian+'c',f.read(1))
+                [n] = struct.unpack(self.endian+'i',f.read(4))
+                if type_r is 'c':
+                    f.seek(n,1)
+                elif type_r is 'i':
+                    f.seek(4*n,1)
+                elif type_r is 'd':
+                    f.seek(8*n,1)
+
+                if tag==''.join(tag_r):
+                    count+=1
+        return count
